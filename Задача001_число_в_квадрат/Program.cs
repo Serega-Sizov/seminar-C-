884daf64ed2ab30fedee9dcf913e4ec5586a3dcf
«Sergei» <seregavil@mail.ru>
2022-12-16 11:16:04 +0300

    Возвели число в квадоат двумя способами

---
+++ Задача001_число_в_квадрат/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 1: Написать программу, которая на вход принимает число и возводит в квадрат
+Console.WriteLine("Введи число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+// Математический метод
+int sqr = number * number;
+//С библиотекой
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+
+Console.WriteLine($"Число {number} в квадрате равно: Математически {sqr}, с использованием библиотеки {sqr1}");
