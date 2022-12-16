7ed69c52b45c6981212fdfde8b8b01979928e072
«Sergei» <seregavil@mail.ru>
2022-12-16 11:28:04 +0300

    Программа принимает число Н и прописывает значения от -Н до Н

---
+++ Задача002_промежуток_от_до/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает одно число (N), 
+//а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Введи число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int num1 = -num;
+
+while(num1 <= num){
+    Console.Write($"{num1} ");
+    num1++;
+}
