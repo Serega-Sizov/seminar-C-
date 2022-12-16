919381526f9db57aa835fc0e0a1526a6d1b98a5e
«Sergei» <seregavil@mail.ru>
2022-12-16 12:29:26 +0300

    программа пишет день недели, когда вводишь число от 1 до 7

---
+++ Задача004_дни_недели/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру. 3 -> Среда 5 ->  Пятница
+Console.Write("Введите число от 1 до 7: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number == 1)
+{
+    Console.Write("Понедельник");
+}
+if (number == 2)
+{
+    Console.Write("Вторник");
+}
+if (number == 3)
+{
+    Console.Write("Среда");
+}
+if (number == 4)
+{
+    Console.Write("Четверг");
+}
+if (number == 5)
+{
+    Console.Write("Пятница");
+}
+if (number == 6)
+{
+    Console.Write("Суббота");
+}
+if (number == 7)
+{
+    Console.Write("Воскресенье");
+}
