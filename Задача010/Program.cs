2f06700b6faed27062cb8c02fcd8d4f99d1a4c7a
«Sergei» <seregavil@mail.ru>
2022-12-19 17:20:57 +0300

    программа принимает 2 числа и проверяет является ли одно число квадратом другого

--- Задача010/Program.cs
+++ Задача010/Program.cs
@@ -7,9 +7,9 @@ int num2 = int.Parse(Console.ReadLine()!);
 
 if (num1 * num1 == num2 || num2 * num2 == num1)
     {
-        Console.Write($"Одно число является квадратом другого числа");
+        Console.Write("Одно число является квадратом другого числа");
     }
 else
     {
-        Console.Write($"Одно число НЕ является квадратом другого числа");
+        Console.Write("Одно число НЕ является квадратом другого числа");
     }
