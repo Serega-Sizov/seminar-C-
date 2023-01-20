fa1f8c3d323fc67d4b229c38fd53749af32fa0de
«Sergei» <seregavil@mail.ru>
2023-01-20 12:24:02 +0300

    программа принимает шестизначное число и проверяет счастливое оно или нет

---
+++ Задача018/Program.cs
@@ -0,0 +1,28 @@
+﻿// Определить, является ли заданное шестизначное число счастливым. 
+// (Счастливым называют такое шестизначное число, что сумма его первых трех цифр равна сумме его последних трех цифр).
+
+Console.Write("Введите шестизначное число: ");
+string num = Console.ReadLine()!;
+
+isHappy(num);
+
+
+// ------МЕТОД-----
+void isHappy (string num)
+{
+    int sum1 = 0;
+    int sum2 = 0;
+    for (int i = 0; i < num.Length/2; i++)
+    {
+        sum1 += Convert.ToInt32(Convert.ToString(num[i]));
+        sum2 += Convert.ToInt32(Convert.ToString(num[num.Length - i - 1]));
+    }
+    if (sum1 == sum2)
+    {
+        Console.WriteLine("Число является счастливым!!!");
+    }
+    else
+    {
+        Console.WriteLine("Попробуйте еще раз");
+    }
+}
